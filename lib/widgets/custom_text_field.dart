4978c86953162073985494fa9573142fe906a488
Your Name <you@example.com>
2025-01-22 05:32:37 +0200

    create custom text field

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/constant.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+          hintText: 'Title',
+          border: buildBorder(),
+          enabledBorder: buildBorder(),
+          focusedBorder: buildBorder(kPrimaryColor)),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+        borderSide: BorderSide(color: color ?? Colors.white));
+  }
+}
