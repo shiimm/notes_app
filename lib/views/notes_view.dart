0739754c54a48c5d7b2f07343687695fab20de24
Your Name <you@example.com>
2025-01-22 04:43:39 +0200

    add border radius to bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -13,6 +13,8 @@ class NotesView extends StatelessWidget {
         backgroundColor: Colors.lightBlue.withOpacity(.9),
         onPressed: () {
           showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)),
               context: context,
               builder: (context) {
                 return const AddNoteBottomSheet();
